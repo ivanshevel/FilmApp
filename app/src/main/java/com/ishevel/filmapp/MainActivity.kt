86fbd8098a6c7cc743da468f286faef5d378a2fe
ishevel <ishevel@gmail.com>
2023-03-08 15:12:29 +0100

    Remove custom implementation of bottom navigation. Clean up unused actions in nav_graph. Little refactor of bootom_menu.xml

--- app/src/main/java/com/ishevel/filmapp/MainActivity.kt
+++ app/src/main/java/com/ishevel/filmapp/MainActivity.kt
@@ -2,13 +2,10 @@ package com.ishevel.filmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.OnBackPressedCallback
 import androidx.core.view.isVisible
-import androidx.navigation.findNavController
-import com.google.android.material.navigation.NavigationBarView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.ishevel.filmapp.databinding.ActivityMainBinding
-import com.ishevel.filmapp.ui.FilmsFavoritesFragmentDirections
-import com.ishevel.filmapp.ui.FilmsListFragmentDirections
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,56 +17,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNavigation.setOnItemSelectedListener(navListener)
-
-        onBackPressedDispatcher.addCallback(this, callback)
-    }
-
-    private val navListener = NavigationBarView.OnItemSelectedListener {
-
-        val fragmentId = findNavController(R.id.nav_host_fragment).currentDestination?.id
-        val navController = findNavController(R.id.nav_host_fragment)
-
-        when (it.itemId) {
-            R.id.bottom_menu_home -> {
-                if (fragmentId == R.id.filmsFavoritesFragment) {
-                    val action =
-                        FilmsFavoritesFragmentDirections.actionFilmsFavoritesFragmentToFilmsListFragment()
-                    navController.navigate(action)
-                }
-            }
-            R.id.bottom_menu_fav -> {
-                if (fragmentId == R.id.filmsListFragment ) {
-                    val action =
-                        FilmsListFragmentDirections.actionFilmsListFragmentToFilmsFavoritesFragment()
-                    navController.navigate(action)
-                }
-            }
-        }
-        true
+        val navHostFragment = supportFragmentManager.
+            findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
     }
 
     fun changeVisibilityOfBottomNavigation(status: Boolean) {
         binding.bottomNavigation.isVisible = status
-    }
-
-    private val callback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
-            val bottomNav = binding.bottomNavigation as NavigationBarView
-            when (findNavController(R.id.nav_host_fragment).previousBackStackEntry?.destination?.id) {
-                R.id.filmsListFragment -> {
-                    bottomNav.menu.getItem(0).isChecked = true
-                }
-                R.id.filmsFavoritesFragment -> {
-                    bottomNav.menu.getItem(1).isChecked = true
-                }
-            }
-            if (isEnabled) {
-                isEnabled = false
-                onBackPressedDispatcher.onBackPressed()
-            }
-            isEnabled = true
-        }
-
     }
 }
